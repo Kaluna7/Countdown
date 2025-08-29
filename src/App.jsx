9b15a906c314fdd98d68058ef0cffe19923d8e11
kaluna7 <kaluna12345678910@gmail.com>
2025-08-29 16:07:05 +0800

    Create the project (Install tailwind css and configure)

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Banner from "./Features/Banner";
+
+export default function App(){
+  return(
+    <div className="bg-green-900 h-screen">
+      <Banner />
+    </div>
+  );
+}
---
+++ src/Features/Banner.jsx
@@ -0,0 +1,10 @@
+export default function Banner(){
+    return(
+        <div className="relative max-w-[75%] h-1/2 bg-black top-8 rounded-4xl left-1/2 transform -translate-x-1/2 flex flex-col items-center p-15">
+            <div className="text-white flex flex-col text-center">
+                <h1 className="text-5xl font-bold font-opensans">THE <span className="text-cyan-400">ALMOST</span> FINAL COUNTDOWN</h1>
+                <p>Stop the timer once you estimate that time (is almost) up</p>
+            </div>
+        </div>
+    );
+} 
